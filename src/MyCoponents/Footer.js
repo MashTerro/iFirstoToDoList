450d8e68536bb367a3abac68f55871c2871295e3
Mahevash Patel <90512296+MashTerro@users.noreply.github.com>
2021-11-21 19:14:13 +0530

    Update Footer.js

--- src/MyCoponents/Footer.js
+++ src/MyCoponents/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 export const Footer = () => {
- 
+
     return (
         <footer className="bg-dark text-light py-4" >
             <p className="text-center ">
-            Copyright &copy;MytodosList.com
-</p>
+                Copyright &copy;iFirstoToDosList.com
+            </p>
         </footer>
     )
 }
