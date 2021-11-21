b5b096fde9bec2106813d1e5cefa79ba011c0960
Mahevash Patel <90512296+MashTerro@users.noreply.github.com>
2021-11-21 19:13:01 +0530

    Update Addtodo.js

--- src/MyCoponents/Addtodo.js
+++ src/MyCoponents/Addtodo.js
@@ -1,37 +1,43 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
-export const Addtodo = ({addTodo}) => {
-    const [title,setTitle]=useState("");
-    const [desc,setDesc]=useState("");
-   
-    const submit =(e)=>{
+export const Addtodo = ({ addTodo }) => {
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
+    const submit = (e) => {
         e.preventDefault();
-        if(!title || !desc){
+        if (!title || !desc) {
             alert("Title or Description can not be blank!!");
         }
-        else{
-        addTodo(title, desc);
-        setTitle("");
-        setDesc("");
+        else {
+            addTodo(title, desc);
+            setTitle("");
+            setDesc("");
         }
     }
     return (
         <div className="container my-3">
-            <h3>Add your works</h3>
+
+            <h3 className="my-3 text-center">MAKE YOUR LIST</h3>
+            <img className="my-4 text-center" src="https://source.unsplash.com/1400x400/?black,pen" class="img-fluid" />
+            <div className="container my-3"></div>
+            {/* <h3 className="my-3 text-center" >Add your ToDos</h3> */}
+            {/* <button className="btn btn-sm btn-dark" >Delete</button> */}
+
             <form onSubmit={submit}>
                 <div className="mb-3">
-                    <label htmlFor="title" className="form-label">Todo Title</label>
-                    <input type="text"  value={title} onChange={(e)=>setTitle(e.target.value)}
-                    className="form-control" id="title" aria-describedby="emailHelp" />
-                    
+                    <label htmlFor="title" className="form-label" >WORK TITLE</label>
+                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}
+                        className="form-control" id="title" aria-describedby="emailHelp" />
+
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="desc" className="form-label">Work Description</label>
-                    <input type="text" value={desc} onChange={(e)=>setDesc(e.target.value)}
-                    className="form-control" id="desc"/>
+                    <label htmlFor="desc" className="form-label">WORK DESCRIPTION</label>
+                    <input type="text" value={desc} onChange={(e) => setDesc(e.target.value)}
+                        className="form-control" id="desc" />
                 </div>
-               
-                <button type="submit" className="btn btn-sm btn-dark">Add Dos</button>
+
+                <button type="submit" className="btn btn-sm btn-dark">Add ToDo</button>
             </form>
         </div>
     )
